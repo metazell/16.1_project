966bd532db250ea917c252c208f6635613e9e86d
metazell <metazell@gmail.com>
2024-04-16 01:27:11 -0500

    16.1

---
+++ app.js
@@ -0,0 +1,63 @@
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    honk() {
+        return "Beep.";
+    }
+
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+
+    revEngine() {
+        return "VROOM!!!";
+    }
+}
+
+class Garage {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.vehicles = [];
+    }
+
+    add(vehicle) {
+        if (!(vehicle instanceof Vehicle)) {
+            return "Only vehicles are allowed in here!";
+        }
+        if (this.vehicles.length >= this.capacity) {
+            return "Sorry, we're full.";
+        }
+        this.vehicles.push(vehicle);
+        return "Vehicle added!";
+    }
+}
+
+// Example usage:
+let garage = new Garage(2);
+console.log(garage.vehicles); // []
+console.log(garage.add(new Car("Hyundai", "Elantra", 2015))); // "Vehicle added!"
+console.log(garage.vehicles); // [Car]
+console.log(garage.add("Taco")); // "Only vehicles are allowed in here!"
+
+console.log(garage.add(new Motorcycle("Honda", "Nighthawk", 2000))); // "Vehicle added!"
+console.log(garage.vehicles); // [Car, Motorcycle]
+
+console.log(garage.add(new Motorcycle("Honda", "Nighthawk", 2001))); // "Sorry, we're full."
